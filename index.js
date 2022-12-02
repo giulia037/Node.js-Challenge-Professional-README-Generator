8aa13df17b1bd113ad7a2af733f6b6641fa0ed12
giulia shelton <giuliashelton6875@gmail.com>
2022-12-02 18:29:12 -0500

    fixed bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require(`inquirer`);
 const fs = require(`fs`);
-const ReadmeOutput = (data) => `## Node.js-Challenge-Professional-README-Generator
+const ReadmeOutput = ({ pTitle, pDescription, pInstallation, pUsageInfo, pContrib, pTests }) => `## Node.js-Challenge-Professional-README-Generator
 
 For this assignment we had to use Node.js to run the readme file. As the developer you want to see the readme file pop up when you run the code node index.js. How did I get this to run the way I wanted? I simply started by taking what I knew from class. I added the basic functions by using what I wrote in my mini project.The most important part was making sure the readme file would show. To allow my computer to generate the readme I added this code- const ReadmeOutput = (data) =>. That allows me to access my data from my readme file and then push that data after the node code has run. However to get the readme file there are a few questions the user would have to answer before proceeding. 
 
@@ -44,7 +44,7 @@ inquirer
 
 // TODO: Create a function to write README file
 .then((data) => {
-    const content = ReadmeOutput(data);
+    const content = ReadmeOutput(answers);
        console.log(data);
        fs.writeFile("readme.md", content,(err) => {
         console.log(err)
